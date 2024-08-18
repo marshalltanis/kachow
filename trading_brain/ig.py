a3d8fe75ad606346f4b64ec9d9b48b85fc1aa0ac
Marshall Tanis <marshalltanis@yahoo.com>
2024-08-17 17:13:52 -0700

    working logged in session

--- trading_brain/ig.py
+++ trading_brain/ig.py
@@ -1,10 +1,12 @@
 import json
 import os
-import logging
-from websockets import client
+import asyncio
+import websocket as ws
 from time import time
 import uuid
 from utils import logger
+import sys
+from time import sleep
 
 
 class IgUsa:
@@ -13,8 +15,11 @@ class IgUsa:
         self.logger = logger.logger()
         self.logger.info("Creating API")
         self.__parse_config()
-        self.pt_ws = client.connect(uri=self.pretrade,logger=self.logger)
-        self.trade_ws = client.connect(uri=self.trade, logger=self.logger)
+        ws.enableTrace(False)
+        self.pt_ws = ws.WebSocket()
+        self.trade_ws = ws.WebSocket()
+        self.pt_ws.connect(url=self.pretrade)
+        self.trade_ws.connect(url=self.trade)
     
     def __parse_config(self):
         if not os.path.exists(self.config):
@@ -27,7 +32,7 @@ class IgUsa:
             self.posttrade = urls["posttrade"]
             self.chart = urls["chart"]
     
-    def create_session(self, ws: client):
+    def create_session(self, ws: ws.WebSocket):
         creds = {
             "CredentialsType": "login",
             "Token": f"{os.getenv('IG_USER')}:{os.getenv('IG_PASS')}"
@@ -36,8 +41,20 @@ class IgUsa:
             "MessageType": "Negotiate",
             "Timestamp": int(time()),
             "SessionId": f"{uuid.uuid4()}",
-            "ClientFlow": "Unspecified",
+            "ClientFlow": "Unsequenced",
             "Credentials": creds
         }
         self.logger.info("Creating Request")
         self.logger.debug(json.dumps(request))
+        ws.send(json.dumps(request))
+    
+    async def handle_incoming(self, ws: ws.WebSocket):
+        while True:
+            try:
+                response = ws.recv()
+                print ("Received '%s'" % json.loads(response))
+                sleep(3)
+            except KeyboardInterrupt:
+                sys.exit(2)
+            except Exception as e:
+                print(f"websocket error: {e}")
--- trading_brain/trading_logic.py
+++ trading_brain/trading_logic.py
@@ -1,8 +1,21 @@
 from ig import IgUsa
+import asyncio
+from websocket import WebSocket
 
-def main():
+
+async def recv_handler(trade_api: IgUsa):
+    task_pt = asyncio.create_task(trade_api.handle_incoming(trade_api.pt_ws))
+    task_trade = asyncio.create_task(trade_api.handle_incoming(trade_api.trade))
+    await task_pt
+    await task_trade
+
+async def main():
     trade_api = IgUsa("demo_config.json")
+    handler = recv_handler(trade_api)
     trade_api.create_session(trade_api.pt_ws)
+    await(handler)
+   
+    
 
 if __name__=="__main__":
-    main()
+    asyncio.run(main())
