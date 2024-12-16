9cbbb082566b088674a7688d48826fbea9b8760d
Marshall Tanis <marshalltanis@yahoo.com>
2024-12-15 19:48:48 -0800

    save place, IG link broken

--- trading_brain/ig.py
+++ trading_brain/ig.py
@@ -7,6 +7,7 @@ import uuid
 from utils import logger
 import sys
 from time import sleep
+import ssl
 
 
 class IgUsa:
@@ -15,11 +16,12 @@ class IgUsa:
         self.logger = logger.logger()
         self.logger.info("Creating API")
         self.__parse_config()
-        ws.enableTrace(False)
-        self.pt_ws = ws.WebSocket()
-        self.trade_ws = ws.WebSocket()
+        ws.enableTrace(True)
+        self.pt_ws = ws.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+        self.trade_ws = ws.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
         self.pt_ws.connect(url=self.pretrade)
         self.trade_ws.connect(url=self.trade)
+        self.session_id= uuid.uuid4()
     
     def __parse_config(self):
         if not os.path.exists(self.config):
@@ -40,11 +42,11 @@ class IgUsa:
         request = {
             "MessageType": "Negotiate",
             "Timestamp": int(time()),
-            "SessionId": f"{uuid.uuid4()}",
+            "SessionId": f"{self.session_id}",
             "ClientFlow": "Unsequenced",
             "Credentials": creds
         }
-        self.logger.info("Creating Request")
+        self.logger.info("Creating login Request")
         self.logger.debug(json.dumps(request))
         ws.send(json.dumps(request))
     
@@ -54,6 +56,23 @@ class IgUsa:
                 response = ws.recv()
                 print ("Received '%s'" % json.loads(response))
                 sleep(3)
+            except KeyboardInterrupt:
+                sys.exit(2)
+            except Exception as e:
+                print(f"websocket error: {e}")
+    
+    async def keep_alive(self, ws: ws.WebSocket):
+        msg = {
+            "MessageType": "Establish",
+            "Timestamp": int(time()),
+            "SessionId": f"{self.session_id}",
+            "KeepaliveInterval": 65000
+        }
+        while True:
+            try:
+                print(f"Sending keep alive msg: {msg} ")
+                ws.send(json.dumps(msg))
+                sleep(50)
             except KeyboardInterrupt:
                 sys.exit(2)
             except Exception as e:
--- trading_brain/trading_logic.py
+++ trading_brain/trading_logic.py
@@ -1,19 +1,45 @@
 from ig import IgUsa
-import asyncio
+import concurrent.futures as cf
 from websocket import WebSocket
+import asyncio
+import sys
 
 
 async def recv_handler(trade_api: IgUsa):
-    task_pt = asyncio.create_task(trade_api.handle_incoming(trade_api.pt_ws))
-    task_trade = asyncio.create_task(trade_api.handle_incoming(trade_api.trade))
+    with cf.ThreadPoolExecutor(max_workers=2):
+        trade_api.handle_incoming(trade_api.pt_ws)
+        task_trade = asyncio.create_task(trade_api.handle_incoming(trade_api.trade_ws))
     await task_pt
     await task_trade
+    return True
+
+def create_sessions(trade_api: IgUsa):
+    print("Creating Sessions (in sync)")
+    trade_api.create_session(trade_api.pt_ws)
+    trade_api.create_session(trade_api.trade_ws)
+    return True
+
+async def keep_alive(trade_api: IgUsa):
+    task_pt = asyncio.create_task(trade_api.keep_alive(trade_api.pt_ws))
+    task_trade = asyncio.create_task(trade_api.keep_alive(trade_api.trade_ws))
+    await task_pt
+    await task_trade
+    return True
 
 async def main():
     trade_api = IgUsa("demo_config.json")
-    handler = recv_handler(trade_api)
-    trade_api.create_session(trade_api.pt_ws)
-    await(handler)
+    print("Creating Session")
+    try:
+        sessions = create_sessions(trade_api)
+    except Exception as e:
+        print(f"Error creating session: {e}")
+    if not sessions:
+        print("Yikes")
+        sys.exit(2)
+    handler = asyncio.create_task(recv_handler(trade_api))
+    alive = asyncio.create_task(keep_alive(trade_api))
+    await handler
+    await alive
    
     
 
