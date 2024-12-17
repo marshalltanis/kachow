023b1d56d7253c6a3ba44ddfdb26537a2fba465d
marsh <marshalltanis@yahoo.com>
2024-12-17 15:00:47 -0800

    weird stuff going on here

--- trading_brain/metatrader4.py
+++ trading_brain/metatrader4.py
@@ -6,10 +6,22 @@ import uuid
 import sys
 import ssl
 
+
+BYTES_TO_RECEIVE = 4096
+
+class Mt4Tick:
+    time: str
+    ask: str
+    bid: str
+    volume: str
+    spread: str
+
+
 class MT4:
     address: str
     port: int
     mt4_connect = None
+    receive_buffer: list
 
 
     def __init__(self, config:str):
@@ -44,4 +56,22 @@ class MT4:
             print("Failed to create session\n")
             return
         print("Connection established with server\n")
+
+
+    def receive_tick_info(self):
+        data = []
+        while True:
+            data_chunk = self.mt4_connect.recv(BYTES_TO_RECEIVE)
+            print(data_chunk)
+            if not data_chunk:
+                break
+            data.append(data_chunk)
+        string_data = b''.join(data).decode("UTF-8")
+        print(string_data)
+        symbols = string_data.strip(',')
+        previous_tick = Mt4Tick()
+        for symbol in symbols:
+            current_field = symbol.split(':')
+            previous_tick[current_field[0]] = current_field[1]
+        return previous_tick
 
--- trading_brain/trading_logic.py
+++ trading_brain/trading_logic.py
@@ -1,45 +1,36 @@
-from ig import IgUsa
+from metatrader4 import MT4
 import concurrent.futures as cf
-from websocket import WebSocket
 import asyncio
 import sys
 
 
-async def recv_handler(trade_api: IgUsa):
-    with cf.ThreadPoolExecutor(max_workers=2):
-        trade_api.handle_incoming(trade_api.pt_ws)
-        task_trade = asyncio.create_task(trade_api.handle_incoming(trade_api.trade_ws))
-    await task_pt
-    await task_trade
+async def recv_handler(trade_api):
+    while True:
+        tick = trade_api.receive_tick_info()
+        if not tick:
+            print("Something bad happened\n")
     return True
 
-def create_sessions(trade_api: IgUsa):
-    print("Creating Sessions (in sync)")
-    trade_api.create_session(trade_api.pt_ws)
-    trade_api.create_session(trade_api.trade_ws)
+
+def create_sessions(trade_api):
+    trade_api.create_session()
     return True
 
-async def keep_alive(trade_api: IgUsa):
-    task_pt = asyncio.create_task(trade_api.keep_alive(trade_api.pt_ws))
-    task_trade = asyncio.create_task(trade_api.keep_alive(trade_api.trade_ws))
-    await task_pt
-    await task_trade
+async def keep_alive(trade_api):
     return True
 
 async def main():
-    trade_api = IgUsa("demo_config.json")
+    trade_api = MT4("demo_config.json")
     print("Creating Session")
     try:
-        sessions = create_sessions(trade_api)
+        trade_api.create_session()
     except Exception as e:
         print(f"Error creating session: {e}")
-    if not sessions:
-        print("Yikes")
-        sys.exit(2)
-    handler = asyncio.create_task(recv_handler(trade_api))
-    alive = asyncio.create_task(keep_alive(trade_api))
-    await handler
-    await alive
+    await recv_handler(trade_api)
+
+    trade_api.disconnect()
+
+
    
     
 
