0db72cd4d57fc8b58782e9a07ac4bdcd0d04d257
marsh <marshalltanis@yahoo.com>
2024-12-19 16:02:03 -0800

    model initialization working before heading into control mode

--- models/RNN.py
+++ models/RNN.py
@@ -1,23 +1,23 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
-import seaborn as sns
-
-from tensorflow import keras
-from keras.models import Sequential
+import os
+from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM, Dropout
 
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import recall_score
 
 STM = 100
 DROPOUT = 0.1
+MODEL_PATH = "./model.keras"
 
-class LSTM:
+class Model:
     def __init__(self, filename: str, layers: int):
-        data = pd.read_csv('EURUSD_H1.csv', index_col="Date", parse_dates=True)
+        data = pd.read_csv(filename, index_col="Date", parse_dates=True)
         training_set = data.iloc[:,1:2].values
         self.sc = MinMaxScaler(feature_range=(0,1))
-        ts_scaled = sc.fit_transform(training_set)
+        ts_scaled = self.sc.fit_transform(training_set)
 
         self.X_train = []
         self.y_train = []
@@ -27,23 +27,44 @@ class LSTM:
         self.X_train, self.y_train = np.array(self.X_train), np.array(self.y_train)
         self.X_train = np.reshape(self.X_train, (self.X_train.shape[0], self.X_train.shape[1], 1))
         self.layers = layers
+
+        print("Starting model creation")
         self.__create_model()
-        self.__create_test_data()
+
+        print("Starting test data creation")
+        self.test_data = self.__create_test_data("..\\data\\AUDUSD_H1.csv")
+
+        print("Evaluating model recall")
+        precision = self.model_accuracy(self.test_data[0], self.test_data[1])
+
+        print("Saving model")
+        self.model.save(MODEL_PATH)
+
+        print("Ready for action")
+
 
     def __create_model(self):
-        model = Sequential()
-        model.add(LSTM(units=STM,return_sequences=True, input_shape=(self.X_train.shape[1], 1)))
-        model.add(Dropout(DROPOUT))
-        for i in range(0..self.layers):
-            model.add(LSTM(units=STM,return_sequences=True))
+        if os.path.exists(MODEL_PATH):
+            print("Found existing model")
+            self.model = load_model(MODEL_PATH)
+        else:
+            print("Creating new model")
+            model = Sequential()
+            model.add(LSTM(units=STM,return_sequences=True, input_shape=(self.X_train.shape[1], 1)))
             model.add(Dropout(DROPOUT))
-        model.add(Dense(units=1))
-        model.compile(optimizer='adam',loss='mean_squared_error')
-        model.fit(self.X_train,self.y_train,epochs=75,batch_size=32)
-        self.model = model
+            for i in range(1, self.layers-1):
+                model.add(LSTM(units=STM,return_sequences=True))
+                model.add(Dropout(DROPOUT))
+            model.add(LSTM(units=STM, return_sequences=False))
+
+            model.add(Dense(units=1))
+            print("Compiling model")
+            model.compile(optimizer='adam',loss='mean_squared_error')
+            model.fit(self.X_train, self.y_train, epochs=75, batch_size=32, verbose=2)
+            self.model = model
 
     def __create_test_data(self, filename: str):
-        test_data = pd.read_csv('AUDUSD_H1.csv', index_col="Date", parse_dates=True)
+        test_data = pd.read_csv(filename, index_col="Date", parse_dates=True)
         real_stock_price = test_data.iloc[:, 1:2].values
 
         dataset_total = pd.concat((test_data['Open'], test_data['Open']), axis = 0)
@@ -68,6 +89,12 @@ class LSTM:
         plt.ylabel('AUDUSD Price')
         plt.legend()
         plt.show()
+
+    def model_accuracy(self, real_stock_price, predicted_stock_price):
+        #recall = recall_score(real_stock_price[60:] , predicted_stock_price)
+        self.plot_prediction(real_stock_price, predicted_stock_price)
+        #print(f"Recall: {recall}")
+        #return recall
     
     def generate_stock_price(self, last_prices):
         prediction = self.model.predict(last_prices)
--- trading_brain/trading_logic.py
+++ trading_brain/trading_logic.py
@@ -1,26 +1,61 @@
 from metatrader4 import MT4
 import multiprocessing
+import os
+import datetime
 import asyncio
 import time
 import sys
+import models.RNN as RNN
+import logging
+import builtins
 
+# Redisgn this into a config
 RUNNING_TASKS = {}
+TRAINING_DATA = "..\\data\EURUSD_H1.csv"
+PROCESS_QUEUE = multiprocessing.Queue()
+RECV_LOG = f"log\\recv_log_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+MODEL_LOG = f"log\\model_log_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+COMMAND_LOG = f"log\\command_log_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
 
-def recv_handler(trade_api):
+def setup_log(filename):
+    file_handler = logging.FileHandler(filename, mode="a", encoding=None, delay=False)
+    file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    file_handler.stream = open(file_handler.baseFilename, mode="a", buffering=1)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    builtins.print = logger.debug
+def recv_handler(trade_api, data_manager):
+    global PROCESS_QUEUE, RECV_LOG
+    setup_log(RECV_LOG)
     while True:
+        if not data_manager["receive"]["run_flag"]:
+            print("Run flag transitioned, exiting program")
+            return
         if trade_api.is_connected():
-            print("Connection Identified")
             tick = trade_api.receive_tick_info()
             if not tick:
                 print("Socket is disconnected")
                 return
+            PROCESS_QUEUE.put(tick)
 
         else:
             print("Disconnected")
             return
 
-def control(trade_api):
+def update_shared_mem(data_manager, process, item: dict):
+    if process not in data_manager:
+        print(f"Creating process info for {process}")
+        data_manager[process] = {"Process": process}
+        data_manager[process].update(item)
+    else:
+        print(f"Adding {item} to {process}")
+        data_manager[process].update(item)
+def control(trade_api, model):
     global RUNNING_TASKS
+    data_manager = multiprocessing.Manager().dict()
+    recv_run_flag = True
+
     while True:
         print("Please enter a command. Type help for more information:")
         command = input()
@@ -30,6 +65,7 @@ def control(trade_api):
                 c - connect to MT4 server
                 d - disconnect from MT4 server
                 s - display current connection status
+                m - generate & train model
                 e - kill program
                 r - disconnect, refetch & train model, reconnect
             """
@@ -41,7 +77,9 @@ def control(trade_api):
             else:
                 trade_api.create_session()
                 if "receive" not in RUNNING_TASKS:
-                    RUNNING_TASKS["receive"] = multiprocessing.Process(target=recv_handler, args=(trade_api, ))
+                    print("Starting receive thread")
+                    update_shared_mem(data_manager, "receive", {"run_flag": recv_run_flag})
+                    RUNNING_TASKS["receive"] = multiprocessing.Process(target=recv_handler, args=(trade_api, data_manager))
                     RUNNING_TASKS["receive"].start()
                 print("New session created")
             continue
@@ -54,16 +92,34 @@ def control(trade_api):
             continue
         if command == "s":
             trade_api.print_stats()
+            if "model" in RUNNING_TASKS:
+                process_alive = RUNNING_TASKS['model'].is_alive()
+                if process_alive:
+                    pid = RUNNING_TASKS['model'].pid
+                    print(f"Process running model - {pid} is alive")
+                else:
+                    print("Process has finished")
+                    RUNNING_TASKS.pop("model")
             continue
+
         if command == "e":
-            if trade_api.is_connected():
-                RUNNING_TASKS["receive"].terminate()
-                RUNNING_TASKS.pop("receive")
+            data_manager["receive"]["run_flag"] = False
+            trade_api.disconnect()
+            RUNNING_TASKS["receive"].join()
+            RUNNING_TASKS.pop("receive")
             return
 
 
 def initialize_model():
-    return True
+    global PROCESS_QUEUE
+    model: RNN.Model = None
+    if not model:
+        if not os.path.exists(TRAINING_DATA):
+            print(f"Check the path of the data folder - {TRAINING_DATA}")
+        model = RNN.Model(TRAINING_DATA, 3)
+        print("Model initialized, waiting for commands")
+    return model
+
 
 def create_sessions(trade_api):
     trade_api.create_session()
@@ -75,7 +131,8 @@ def keep_alive(trade_api):
 def main():
     global RUNNING_TASKS
     trade_api = MT4("demo_config.json")
-    control(trade_api)
+    model = initialize_model()
+    control(trade_api, model)
 
 if __name__ == "__main__":
     main()
