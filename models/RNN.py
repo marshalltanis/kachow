2f641950185e60f967ba0828dae5d4b22086ab05
marsh <marshalltanis@yahoo.com>
2024-12-21 23:04:47 -0800

    pretty high stats and graph looks nice

--- models/RNN.py
+++ models/RNN.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import os
 from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
 from scipy.stats import linregress
 
 from sklearn.preprocessing import MinMaxScaler
@@ -11,14 +13,19 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, m
 
 
 STM = 100
-DROPOUT = 0.1
+DROPOUT = 0.2
+LEARNING_RATE = 0.0005
+BATCH_SIZE = 32
+EPOCHS = 50
+SEQUENCE_LENGTH = 100
+
 MODEL_PATH = "./model.keras"
 
 class Model:
-    mae_threshold = 0.0012
-    rmse_threshold = 0.0018
-    r2_threshold = 0.6
-    mape_threashold = 0.5
+    mae_threshold = 0.015
+    rmse_threshold = 0.02
+    r2_threshold = 0.9
+    mape_threashold = 4
     test_data = "..\\data\\AUDUSD_H1.csv"
     def __init__(self, filename: str, layers: int, recreate: bool):
         data = pd.read_csv(filename, index_col="Date", parse_dates=True)
@@ -28,8 +35,8 @@ class Model:
 
         self.X_train = []
         self.y_train = []
-        for i in range(60, 2000):
-            self.X_train.append(ts_scaled[i-60:i, 0])
+        for i in range(SEQUENCE_LENGTH, len(ts_scaled)):
+            self.X_train.append(ts_scaled[i-SEQUENCE_LENGTH:i, 0])
             self.y_train.append(ts_scaled[i, 0])
         self.X_train, self.y_train = np.array(self.X_train), np.array(self.y_train)
         self.X_train = np.reshape(self.X_train, (self.X_train.shape[0], self.X_train.shape[1], 1))
@@ -45,8 +52,9 @@ class Model:
         model_ready = self.validate_model(self.test_data[0], self.test_data[1])
 
         if not model_ready:
-            print("Need to tune the model more...")
-            return
+            print("Need to tune the model more... Save anyways?")
+            if input() != 'y':
+                return
         print("Saving model")
         self.model.save(MODEL_PATH)
 
@@ -68,9 +76,17 @@ class Model:
             model.add(LSTM(units=STM, return_sequences=False))
 
             model.add(Dense(units=1))
+
             print("Compiling model")
-            model.compile(optimizer='adam',loss='mean_squared_error')
-            model.fit(self.X_train, self.y_train, epochs=75, batch_size=32, verbose=2)
+            optimizer = Adam(learning_rate=LEARNING_RATE)
+            model.compile(optimizer=optimizer , loss='mean_squared_error')
+            early_stopping = EarlyStopping(
+                monitor='loss',
+                patience=10,
+                min_delta=0.001,
+                restore_best_weights=True
+            )
+            model.fit(self.X_train, self.y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks=[early_stopping])
             self.model = model
 
     def create_test_data(self, filename: str):
@@ -78,12 +94,12 @@ class Model:
         real_stock_price = test_data.iloc[:, 1:2].values
 
         dataset_total = pd.concat((test_data['Open'], test_data['Open']), axis = 0)
-        inputs = dataset_total[len(dataset_total) - len(test_data) - 60:].values
+        inputs = dataset_total[len(dataset_total) - len(test_data) - SEQUENCE_LENGTH:].values
         inputs = inputs.reshape(-1,1)
         inputs = self.sc.transform(inputs)
         X_test = []
-        for i in range(60, 100000):
-            X_test.append(inputs[i-60:i, 0])
+        for i in range(SEQUENCE_LENGTH, 100000):
+            X_test.append(inputs[i-SEQUENCE_LENGTH:i, 0])
 
         X_test = np.array(X_test)
         X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
@@ -101,13 +117,14 @@ class Model:
         plt.show()
 
     def validate_model(self, real_stock_price, predicted_stock_price):
-        mae = mean_absolute_error(real_stock_price[:-60], predicted_stock_price)
-        mse = mean_squared_error(real_stock_price[:-60], predicted_stock_price)
+        mae = mean_absolute_error(real_stock_price[SEQUENCE_LENGTH:], predicted_stock_price)
+        mse = mean_squared_error(real_stock_price[SEQUENCE_LENGTH:], predicted_stock_price)
         rmse = np.sqrt(mse)
-        r2 = r2_score(real_stock_price[:-60], predicted_stock_price)
-        mape = mean_absolute_percentage_error(real_stock_price[:-60], predicted_stock_price) * 100
+        r2 = r2_score(real_stock_price[SEQUENCE_LENGTH:], predicted_stock_price)
+        mape = mean_absolute_percentage_error(real_stock_price[SEQUENCE_LENGTH:], predicted_stock_price) * 100
 
         print(f"Mean Absolute Error: {mae} - Mean Absolute Percentage Error: {mape} - Root Mean Squared Error: {rmse} - R-squared: {r2}")
+        self.plot_prediction(real_stock_price, predicted_stock_price)
 
         if rmse >= self.rmse_threshold:
             print(f"RMSE threshold violated: {rmse}")
